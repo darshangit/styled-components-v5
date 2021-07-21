a57e400226f70ed0042366f13e5deb84548c25dc
Darshan Satya <darshans1989@gmail.com>
2021-07-20 20:16:16 -0400

    Button interpolation and css helper

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import { Button } from "components/common";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script';
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>App</h1>
-      <Button>TEST</Button>
+      <Button>Primary</Button>
+      <Button secondary>Secondary</Button>
+      <Button disabled>Disabled</Button>
+      <Button large>Large Button</Button>
     </>
   );
 }
