869eab45e7ac940bcdc7192a8c54de022ce50fb0
Darshan Satya <darshans1989@gmail.com>
2021-07-22 20:38:54 -0400

    header component styled + toggling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { Link as ReactRouterDomLink } from "react-router-dom";
+import { Link as ReactRouterDomLink, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const HeaderWrapper = styled.header`
   height: 60px;
@@ -9,19 +10,33 @@ const HeaderWrapper = styled.header`
   padding: 0 16px;
   position: fixed;
   top: 0;
-  background: #eeeeee;
+  background-image: linear-gradient(to right, #f8049c, #fdd54f);
+  border-bottom: 3px solid #fdd54f;
 `;
 
 const Menu = styled.nav`
-  display: flex;
-  position: relative;
-  width: relative;
+  display: ${(p) => (p.open ? "block" : "none")};
   border-bottom: none;
-  margin: auto 0 auto auto;
   font-family: "Open Sans";
-  background: none;
-  left: initial;
-  top: initial;
+  position: absolute;
+  width: 100%;
+  top: 60px;
+  left: 0;
+  padding: 8px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #fdd54f;
+  background: white;
+
+  @media (min-width: 768px) {
+    display: flex;
+    background: none;
+    left: initial;
+    top: initial;
+    margin: auto 0 auto auto;
+    position: relative;
+    width: initial;
+    border-bottom: none;
+  }
 `;
 
 const Link = ({ isActive, children, ...props }) => {
@@ -34,15 +49,45 @@ const StyledLink = styled(Link)`
   text-align: center;
   box-sizing: border-box;
   margin: auto 0;
+  color: black;
   font-weight: ${(p) => (p.isActive ? "bold" : "normal")};
 `;
 
+const MobileMenuIcon = styled.div`
+  margin: auto 0 auto auto;
+  width: 25px;
+  min-width: 25px;
+  padding: 5px;
+  cursor: pointer;
+
+  > div {
+    height: 3px;
+    background: black;
+    margin: 5px 0;
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
 export const Header = () => {
+  const { pathname } = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <HeaderWrapper>
-      <Menu>
-        <StyledLink to="/">Home</StyledLink>
-        <StyledLink to="login" isActive>
+      <MobileMenuIcon onClick={() => setMenuOpen((s) => !s)}>
+        <div />
+        <div />
+        <div />
+      </MobileMenuIcon>
+      <Menu open={menuOpen}>
+        <StyledLink to="/" isActive={pathname === "/"}>
+          Home
+        </StyledLink>
+        <StyledLink to="login" isActive={pathname === "/login"}>
           Login
         </StyledLink>
       </Menu>
