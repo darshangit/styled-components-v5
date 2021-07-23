d828eaa371a6cf79c1a077d9e8d616fa05dd7a7a
Darshan Satya <darshans1989@gmail.com>
2021-07-22 21:01:39 -0400

    login + form components created

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,54 @@
 import { PageLayout } from "components/common";
 
 const Home = () => {
-  return <PageLayout>Home</PageLayout>;
+  return (
+    <PageLayout>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis
+        libero nec orci tincidunt dapibus. Morbi aliquet purus magna, et
+        vestibulum enim convallis id. Nam sed odio in metus commodo suscipit.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Vestibulum ac quam urna. Nulla convallis est est, eget
+        feugiat libero sagittis at. Mauris sed placerat tellus, ut varius
+        ligula. Quisque sem nisi, tincidunt eget ornare at, lobortis mollis
+        nulla. Donec tincidunt, ex non vestibulum ullamcorper, augue ipsum
+        interdum sapien, at tincidunt augue quam at purus. Sed pretium nisl
+        libero, nec facilisis odio accumsan et. Sed porttitor ex ac arcu
+        malesuada, quis vehicula dolor interdum. Morbi quis dictum nunc. Nam
+        dignissim justo a mollis sagittis. Duis tempor lacus nec lacus iaculis
+        viverra. Curabitur eleifend, tellus tincidunt porta facilisis, sapien
+        lorem condimentum dolor, eget accumsan felis sem nec erat. Morbi
+        bibendum est eu tempor gravida. Ut ac felis scelerisque leo eleifend
+        porttitor. Suspendisse at aliquet nulla. Suspendisse rutrum, arcu non
+        bibendum suscipit, nisi dui sodales orci, a lobortis diam enim quis
+        odio. Mauris consectetur, velit at convallis venenatis, est odio
+        pharetra turpis, a semper diam enim et est. Suspendisse porttitor
+        faucibus lectus, non sagittis augue facilisis ut. Mauris ut nisl
+        pulvinar, cursus mauris a, volutpat dolor. Fusce cursus, ipsum porta
+        consectetur facilisis, metus orci faucibus diam, id tincidunt leo libero
+        ut tortor. Vivamus dapibus ligula accumsan turpis laoreet, sed auctor
+        lorem semper. Nam vitae sodales ligula. Sed eget lorem ullamcorper,
+        volutpat quam sed, sagittis lacus. Nulla imperdiet sapien ut orci
+        dapibus, et laoreet ex tempus. Donec pellentesque risus vel ultrices
+        sodales. Aliquam sollicitudin, est non vestibulum condimentum, elit quam
+        eleifend purus, fermentum dapibus nunc ex eu nunc. Donec volutpat dui et
+        tempus faucibus. Mauris quis volutpat magna. Maecenas in eros vel nibh
+        tristique mollis. Curabitur id mauris vel mi vehicula consectetur at nec
+        tellus. Integer nec eleifend lectus. Aenean at nisi suscipit, semper sem
+        eget, viverra purus. Nulla facilisi. Pellentesque ac nisi viverra,
+        lobortis tellus accumsan, laoreet sem. Suspendisse laoreet hendrerit
+        placerat. In vel magna sed ipsum interdum porttitor. Sed in urna
+        scelerisque, vestibulum nibh non, ornare enim. Mauris semper libero sed
+        tristique consequat. Aliquam lorem ligula, dignissim eget magna
+        ultrices, pellentesque fermentum leo. Duis rhoncus, libero ut auctor
+        feugiat, odio mauris congue enim, non eleifend lectus magna et orci.
+        Phasellus porta molestie tellus ut scelerisque. Nullam ut luctus nibh.
+        Nunc consequat malesuada ipsum, nec eleifend velit vulputate vel.
+        Vivamus nec tristique felis.
+      </p>
+    </PageLayout>
+  );
 };
 
 export default Home;
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,50 @@
-import { PageLayout } from "components/common";
+import { PageLayout, Input } from "components/common";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1px solid #eee;
+  padding: 16px;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 4px;
+`;
 
 const Login = () => {
-  return <PageLayout>Login</PageLayout>;
+  const [formFields, setFormFields] = useState({ username: "", password: "" });
+
+  const handleInputChange = (event) => {
+    event.persist();
+    setFormFields((s) => ({
+      ...s,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return (
+    <PageLayout>
+      <h1>Login</h1>
+      <Form>
+        <Input
+          value={formFields.username}
+          onChange={handleInputChange}
+          name="username"
+          type="text"
+          placeholder="Username"
+        />
+        <Input
+          value={formFields.password}
+          onChange={handleInputChange}
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+      </Form>
+    </PageLayout>
+  );
 };
 
 export default Login;
